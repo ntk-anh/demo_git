6d27b9594ba75b1148b37af85ee0e5e29638eb81
Nguyễn Thị Kim Ánh <anh.ntk.998@gmail.com>
2022-09-10 16:28:38 +0700

    first commit

---
+++ src/main/java/com/cybersoft/git/controller/HomeController.java
@@ -0,0 +1,34 @@
+package com.cybersoft.git.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+	// ModelAndView :+ tạo giao diện của 1 đường dẫn nào đó
+	// + trả ra giá trị cho giao diện xài giống jsp
+	@GetMapping("")
+	public ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView("home");// tên file
+		
+		int soA=10;
+		int soB=20;
+		int tong=tinhTong(soA, soB);
+		
+		modelAndView.addObject("ketqua",tong);
+		
+		
+		
+		return modelAndView;
+	}
+
+	public int tinhTong(int soA, int soB) {
+		int tong = 0;
+		tong = soA + soB;
+		return tong;
+	}
+}
